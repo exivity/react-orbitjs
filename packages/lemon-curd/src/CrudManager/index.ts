eced71006cbdb58786851e49311ba85ccbe91d2e
Michiel <Michiel_87@hotmail.com>
2019-04-16 15:36:21 +0200

    Provide default option

--- packages/lemon-curd/src/CrudManager/index.ts
+++ packages/lemon-curd/src/CrudManager/index.ts
@@ -64,7 +64,7 @@ export class CrudManager {
       onUpdate,
       onError,
       ...options
-    }: Options
+    }: Options = {}
   ) => {
     const { createRecord, updateRecord } = this._operations
     
