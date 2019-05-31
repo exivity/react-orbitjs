dcce59a8e38c391bf540b0a961ffade2c60dc815
Joram van den Boezem <joram@vandenboezem.nl>
2019-05-31 12:23:34 +0200

    feat: add orbit-type-generator

---
+++ packages/orbit-type-generator/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'roots': [
+    '<rootDir>/src'
+  ],
+  'transform': {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  moduleFileExtensions: ['js', 'ts', 'json']
+}
---
+++ packages/orbit-type-generator/src/index.ts
@@ -0,0 +1,5 @@
+import { Schema } from '@orbit/data'
+
+export function generateTypes (schema: Schema) {
+  return schema
+}
