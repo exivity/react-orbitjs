0b8c3604af50353d0809f272d438d4009b95d703
Joram van den Boezem <joram@vandenboezem.nl>
2021-08-03 22:07:50 +0200

    chore: update test utils

--- src/__tests__/utils/index.js
+++ src/__tests__/utils/index.js
@@ -2,9 +2,14 @@ import * as Data from "@orbit/data"
 import * as Memory from "@orbit/memory"
 import * as Records from "@orbit/records"
 
+export function getOrbitVersion() {
+  return typeof Memory.MemorySource === 'undefined'
+    ? 16
+    : 17
+}
+
 export function getOrbitSchemaAndStore(models) {
-  // console.log({ Data, Memory, Records })
-  if (typeof Memory.MemorySource === 'undefined') {
+  if (getOrbitVersion() === 16) {
     // OrbitJs 0.16.x
     const schema = new Data.Schema(models)
     return {
